00e8dde702e3d5bc960f840eca439ed7ba23109b
Forest Collins Sharp <forestcollinssharp@gmail.com>
2024-07-27 10:47:04 -0500

    Push flatbuffers as a regular directory

---
+++ flatbuffers/build/CMakeFiles/flatsampletext.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/forest/Desktop/blender_live_link/flatbuffers/samples/sample_text.cpp" "CMakeFiles/flatsampletext.dir/samples/sample_text.cpp.o" "gcc" "CMakeFiles/flatsampletext.dir/samples/sample_text.cpp.o.d"
+  "/Users/forest/Desktop/blender_live_link/flatbuffers/src/idl_gen_text.cpp" "CMakeFiles/flatsampletext.dir/src/idl_gen_text.cpp.o" "gcc" "CMakeFiles/flatsampletext.dir/src/idl_gen_text.cpp.o.d"
+  "/Users/forest/Desktop/blender_live_link/flatbuffers/src/idl_parser.cpp" "CMakeFiles/flatsampletext.dir/src/idl_parser.cpp.o" "gcc" "CMakeFiles/flatsampletext.dir/src/idl_parser.cpp.o.d"
+  "/Users/forest/Desktop/blender_live_link/flatbuffers/src/reflection.cpp" "CMakeFiles/flatsampletext.dir/src/reflection.cpp.o" "gcc" "CMakeFiles/flatsampletext.dir/src/reflection.cpp.o.d"
+  "/Users/forest/Desktop/blender_live_link/flatbuffers/src/util.cpp" "CMakeFiles/flatsampletext.dir/src/util.cpp.o" "gcc" "CMakeFiles/flatsampletext.dir/src/util.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ flatbuffers/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/forest/Desktop/blender_live_link/flatbuffers
+# Build directory: /Users/forest/Desktop/blender_live_link/flatbuffers/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[flattests]=] "/Users/forest/Desktop/blender_live_link/flatbuffers/build/flattests")
+set_tests_properties([=[flattests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/forest/Desktop/blender_live_link/flatbuffers/CMakeLists.txt;688;add_test;/Users/forest/Desktop/blender_live_link/flatbuffers/CMakeLists.txt;0;")
