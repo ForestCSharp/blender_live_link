e76920779431e54b9ad579e78fb0a97288d77e21
Forest Collins Sharp <forestcollinssharp@gmail.com>
2024-10-18 09:49:45 -0600

    Rudimentary Live Update
    Only supports new objects and has to be manually pushed

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 # Reminder to self: Need to zip up folder to install as add-on
 
+# legacy bl_info Now using blender_manifest.toml
 #bl_info = {
 #    "name": "Blender Live Link",
 #    "blender": (4, 00, 0),
@@ -14,7 +15,9 @@ import socket
 from os.path import dirname, realpath, basename, isfile, join
 import glob
 
-sys.path.append(dirname(realpath(__file__)) + "/compiled_schemas/python") 
+path_to_append = dirname(realpath(__file__)) + "/compiled_schemas/python"
+if path_to_append not in sys.path:
+    sys.path.append(path_to_append)
 
 from .compiled_schemas.python import flatbuffers
 from .compiled_schemas.python.Blender.LiveLink import Update
@@ -25,111 +28,129 @@ from .compiled_schemas.python.Blender.LiveLink import Vec3
 from .compiled_schemas.python.Blender.LiveLink import Quat
 from .compiled_schemas.python.Blender.LiveLink import Vertex
 
+def is_socket_connected(socket):
+    try:
+        socket.getpeername()
+        return True
+    except OSError:
+        return False
+
 class BlenderLiveLinkInit(bpy.types.Operator):
-    """ Blender Live Link Init """          # Use this as a tooltip for menu items and buttons.
-    bl_idname = "live_link.init"            # Unique identifier for buttons and menu items to reference.
-    bl_label = "Live Link: Init"            # Display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}       # Enable undo for the operator.
+    """ Blender Live Link Send Update """       # Use this as a tooltip for menu items and buttons.
+    bl_idname = "live_link.send_update"         # Unique identifier for buttons and menu items to reference.
+    bl_label = "Live Link: Send Update"         # Display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}           # Enable undo for the operator.
 
-    def execute(self, context):
+    #FCS TODO: Store magic IP and Port numbers in some shared file
+    HOST = '127.0.0.1'
+    PORT = 65432
 
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def make_flatbuffer_object(self, builder, obj, dependency_graph):
+        # Allocate string for object name
+        object_name = builder.CreateString(obj.name)
+
+        # If we're a mesh object, setup mesh data 
+        mesh = None
+
+        if (obj.type == 'MESH'): 
+            # Evaluate Modifiers
+            obj_evaluated = obj.evaluated_get(dependency_graph)
+            # Get Mesh Data
+            mesh = obj_evaluated.data
+
+            # Make sure Mesh is triangulated
+            bm = bmesh.new()
+            bm.from_mesh(mesh)
+            bmesh.ops.triangulate(bm, faces=bm.faces[:])
+            bm.to_mesh(mesh)
+
+            # Export Vertices
+            blender_vertices = mesh.vertices
+            
+            Mesh.MeshStartVerticesVector(builder, len(blender_vertices))
+            for blender_vertex in reversed(blender_vertices):
+                position = blender_vertex.co.to_4d()
+                normal = blender_vertex.normal.to_4d()
+                Vertex.CreateVertex(
+                    builder,
+                    position.x, position.y, position.z, position.w,
+                    normal.x, normal.y, normal.z, normal.w
+                )
+            mesh_vertices = builder.EndVector()
+
+            # Export Indices
+            indices = []
+            for blender_polygon in mesh.polygons:
+                # Should be able to assume triangles here because of triangulate call above
+                indices.append(blender_polygon.vertices[0])
+                indices.append(blender_polygon.vertices[1])
+                indices.append(blender_polygon.vertices[2])
+
+            Mesh.MeshStartIndicesVector(builder, len(indices))
+            for index in reversed(indices):
+                builder.PrependUint32(index)
+            mesh_indices = builder.EndVector()
+
+            Mesh.Start(builder)
+            Mesh.AddVertices(builder, mesh_vertices)
+            Mesh.AddIndices(builder, mesh_indices)
+            mesh = Mesh.End(builder)
+        
+        # Begin New Object 
+        Object.Start(builder)
+        
+        # Object Name
+        Object.AddName(builder, object_name)
+
+        # Session UID (note that this is a fairly new addition to the python API)
+        session_uid = obj.session_uid
+        Object.AddUniqueId(builder, session_uid)
+
+        is_visible = obj.visible_get()
+        Object.AddVisibility(builder, is_visible)
+
+        # Object Location
+        location_vec3 = Vec3.CreateVec3(builder, obj.location.x, obj.location.y, obj.location.z)
+        Object.AddLocation(builder, location_vec3)
+
+        # Object Scale
+        scale_vec3 = Vec3.CreateVec3(builder, obj.scale.x, obj.scale.y, obj.scale.z)
+        Object.AddScale(builder, scale_vec3)
+
+        # Object Rotation
+        rot = obj.rotation_euler.to_quaternion()
+        rotation_quat = Quat.CreateQuat(builder, rot.x, rot.y, rot.z, rot.w)
+        Object.AddRotation(builder, rotation_quat)
+
+        # Object Mesh Data
+        if (mesh != None):
+            Object.AddMesh(builder, mesh)
+
+        # End New Object add add to array
+        live_link_object = Object.End(builder)
+
+        return live_link_object
+
+    # Creates an update for objects in in_object_list
+    def make_update(self, in_object_list):
         # Evaluate Depsgraph
-        dg = bpy.context.evaluated_depsgraph_get()
+        dependency_graph = bpy.context.evaluated_depsgraph_get()
 
         # init flatbuffers builder
         builder = flatbuffers.Builder(0)
 
-        #FCS TODO: Smarter way to iterate collection hierarchy to maintain parentage?
         # Build up objects to be added to scene objects vector
         live_link_objects = []
-        for obj in bpy.context.scene.objects: 
-            # Allocate string for object name
-            object_name = builder.CreateString(obj.name)
-
-            # If we're a mesh object, setup mesh data 
-            mesh = None
-            if (obj.type == 'MESH'): 
-                # Evaluate Modifiers
-                obj_evaluated = obj.evaluated_get(dg)
-                # Get Mesh Data
-                mesh = obj_evaluated.data
-
-                # Make sure Mesh is triangulated
-                bm = bmesh.new()
-                bm.from_mesh(mesh)
-                bmesh.ops.triangulate(bm, faces=bm.faces[:])
-                bm.to_mesh(mesh)
-
-                # Export Vertices
-                blender_vertices = mesh.vertices;
-                
-                Mesh.MeshStartVerticesVector(builder, len(blender_vertices))
-                for blender_vertex in reversed(blender_vertices):
-                    position = blender_vertex.co.to_4d()
-                    normal = blender_vertex.normal.to_4d()
-                    Vertex.CreateVertex(
-                        builder,
-                        position.x, position.y, position.z, position.w,
-                        normal.x, normal.y, normal.z, normal.w
-                    )
-                mesh_vertices = builder.EndVector()
-
-                # Export Indices
-                indices = []
-                for blender_polygon in mesh.polygons:
-                    #FCS TODO: Can't assume triangles here...
-                    indices.append(blender_polygon.vertices[0])
-                    indices.append(blender_polygon.vertices[1])
-                    indices.append(blender_polygon.vertices[2])
-
-                Mesh.MeshStartIndicesVector(builder, len(indices))
-                for index in reversed(indices):
-                    builder.PrependUint32(index)
-                mesh_indices = builder.EndVector()
- 
-                Mesh.Start(builder)
-                Mesh.AddVertices(builder, mesh_vertices)
-                Mesh.AddIndices(builder, mesh_indices)
-                mesh = Mesh.End(builder)
-            
-            # Begin New Object 
-            Object.Start(builder)
-            
-            # Object Name
-            Object.AddName(builder, object_name)
-
-            # Session UID (note that this is a fairly new addition to the python API)
-            session_uid = obj.session_uid
-            Object.AddUniqueId(builder, session_uid)
-
-            is_visible = obj.visible_get()
-            Object.AddVisibility(builder, is_visible)
-
-            # Object Location
-            location_vec3 = Vec3.CreateVec3(builder, obj.location.x, obj.location.y, obj.location.z)
-            Object.AddLocation(builder, location_vec3)
-
-            # Object Scale
-            scale_vec3 = Vec3.CreateVec3(builder, obj.scale.x, obj.scale.y, obj.scale.z)
-            Object.AddScale(builder, scale_vec3)
-
-            # Object Rotation
-            rot = obj.rotation_euler.to_quaternion();
-            rotation_quat = Quat.CreateQuat(builder, rot.x, rot.y, rot.z, rot.w);
-            Object.AddRotation(builder, rotation_quat);
-
-            # Object Mesh Data
-            if (mesh != None):
-                Object.AddMesh(builder, mesh)
-
-            # End New Object add add to array
-            live_link_object = Object.End(builder)
-            live_link_objects.append(live_link_object)
+        for blender_object in in_object_list: 
+            live_link_objects.append(self.make_flatbuffer_object(builder, blender_object, dependency_graph))
 
         # actually create the scene objects vector
         Update.UpdateStartObjectsVector(builder, len(live_link_objects))
         for live_link_object in live_link_objects: 
             builder.PrependUOffsetTRelative(live_link_object)
+
         scene_objects = builder.EndVector()
 
         # create string for scene name
@@ -144,18 +165,29 @@ class BlenderLiveLinkInit(bpy.types.Operator):
         # finalize scene flatbuffer
         live_link_scene = Update.End(builder)
 
-        builder.Finish(live_link_scene)
+        builder.FinishSizePrefixed(live_link_scene)
         
-        flatbuffer_data = builder.Output()
+        return builder.Output()
 
-	    #FCS TODO: Store magic IP and Port numbers in some shared file
-        HOST = '127.0.0.1'
-        PORT = 65432
 
-        my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        my_socket.connect((HOST,PORT))
-        my_socket.send(flatbuffer_data)
-        my_socket.shutdown(socket.SHUT_RDWR)
+    # Called when operator is run
+    def execute(self, context):
+
+        if not is_socket_connected(self.my_socket):
+            self.my_socket.connect((self.HOST,self.PORT))
+
+        #FCS TODO: Smarter way to iterate collection hierarchy to maintain parentage?
+        # Create Initial flatbuffer update message
+        flatbuffer_update = self.make_update(bpy.context.scene.objects);
+        self.my_socket.send(flatbuffer_update)
+
+        # Testing sending one object at a time
+        #for blender_object in bpy.context.scene.objects:
+        #    flatbuffer_update = self.make_update([blender_object]);
+        #    my_socket.send(flatbuffer_update)
+
+        # FCS TODO: Remove Shutdown
+        #my_socket.shutdown(socket.SHUT_RDWR)
 
         return {'FINISHED'}
 
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,6 +2,10 @@
 #include <atomic>
 #include <cerrno>
 #include <cstdio>
+
+#include <optional>
+using std::optional;
+
 #include <thread>
 
 #include "ankerl/unordered_dense.h"
@@ -55,11 +59,9 @@ typedef uint16_t 	u16;
 typedef uint8_t 	u8;
 
 struct Mesh {
-	HMM_Vec4 location;
 	u32 idx_count;
-	sg_buffer vbuf;
-	sg_buffer ibuf;
-	sg_buffer sbuf;
+	sg_buffer index_buffer; 
+	sg_buffer vertex_buffer; 
 };
 
 struct Vertex
@@ -69,24 +71,13 @@ struct Vertex
 };
 
 Mesh make_mesh(
-	HMM_Vec4 location, 
-	HMM_Vec4 scale, 
-	HMM_Quat rotation, 
 	Vertex* vertices, 
 	u32 vertices_len, 
 	u32* indices, 
 	u32 indices_len
 )
 {
-	sg_buffer vbuf = sg_make_buffer((sg_buffer_desc){
-		.data = {
-			.ptr = vertices,
-			.size = vertices_len * sizeof(Vertex)
-		},
-		.label = "mesh-vertices"
-	});
-
-	sg_buffer ibuf = sg_make_buffer((sg_buffer_desc){
+	sg_buffer index_buffer = sg_make_buffer((sg_buffer_desc){
 		.type = SG_BUFFERTYPE_INDEXBUFFER,
 		.data = {
 			.ptr = indices,
@@ -95,6 +86,40 @@ Mesh make_mesh(
 		.label = "mesh-indices"
 	});
 
+	sg_buffer vertex_buffer = sg_make_buffer((sg_buffer_desc){
+		.data = {
+			.ptr = vertices,
+			.size = vertices_len * sizeof(Vertex)
+		},
+		.label = "mesh-vertices"
+	});
+
+	return (Mesh) {
+		.idx_count = indices_len,
+		.index_buffer = index_buffer,
+		.vertex_buffer = vertex_buffer,
+	};
+}
+
+struct Object {
+	i32 unique_id;
+	bool visibility;
+
+	sg_buffer storage_buffer; 
+
+	// Mesh Data, stored inline
+	bool has_mesh;
+	Mesh mesh;
+};
+
+Object make_object(
+	i32 unique_id,	
+	bool visibility,
+	HMM_Vec4 location, 
+	HMM_Vec4 scale, 
+	HMM_Quat rotation
+)
+{
 	// From shader header
 	ObjectData_t object_data = {};
 	HMM_Mat4 scale_matrix = HMM_Scale(HMM_V3(scale.X, scale.Y, scale.Z));
@@ -102,18 +127,18 @@ Mesh make_mesh(
 	HMM_Mat4 translation_matrix = HMM_Translate(HMM_V3(location.X, location.Y, location.Z));
 	object_data.model_matrix = HMM_MulM4(translation_matrix, HMM_MulM4(rotation_matrix, scale_matrix));	
 
-	sg_buffer sbuf = sg_make_buffer((sg_buffer_desc){
+	sg_buffer storage_buffer = sg_make_buffer((sg_buffer_desc){
 		.type = SG_BUFFERTYPE_STORAGEBUFFER,
 		.data = SG_RANGE(object_data),
-		.label = "mesh-storage-buffer"
+		.label = "object-storage-buffer"
 	});
 
-	return (Mesh) {
-		.location = location,
-		.idx_count = indices_len,
-		.vbuf = vbuf,
-		.ibuf = ibuf,
-		.sbuf = sbuf,
+	return (Object) {
+		.unique_id = unique_id,
+		.visibility = visibility,
+		.storage_buffer = storage_buffer,
+		.has_mesh = false,
+		.mesh = {},
 	};
 }
 
@@ -125,7 +150,7 @@ struct Camera {
 
 struct {
     sg_pipeline pipeline;
-	sbuffer(Mesh) meshes;
+	map<i32, Object> objects;
 	int blender_socket;
 	std::atomic<bool> blender_data_loaded = false;
 	Camera camera = {
@@ -159,134 +184,178 @@ void live_link_init()
 	socklen_t addr_size = sizeof their_addr;
 	int connection_socket = accept(state.blender_socket, (struct sockaddr *) &their_addr, &addr_size);
 
-	sbuffer(u8) flatbuffer_data = nullptr;
-
-	int bytes_read = 0;
-	int packets_read = 0;
-	do 
+	// infinite recv loop
+	while (true)
 	{
-		const size_t buffer_len = 4096; 
-		u8 buffer[buffer_len];
-		const int flags = 0;
-		bytes_read = recv(connection_socket, buffer, buffer_len, flags);
+		sbuffer(u8) flatbuffer_data = nullptr;
 
-		if (bytes_read < 0)
+		int current_bytes_read = 0;
+		int total_bytes_read = 0;
+		int packets_read = 0;
+		optional<flatbuffers::uoffset_t> flatbuffer_size;
+		do 
 		{
-			printf("recv_error: %s\n", strerror(errno));
-			exit(0);
-		}
-		
-		if (bytes_read > 0)
-		{
-			i32 next_idx = arrlen(flatbuffer_data);
-			arraddn(flatbuffer_data, bytes_read);
-			memcpy(&flatbuffer_data[next_idx], buffer, bytes_read);
-			++packets_read;
-		}
-	}
-	while (bytes_read > 0);
+			const size_t buffer_len = 4096; 
+			u8 buffer[buffer_len];
+			const int flags = 0;
+			current_bytes_read = recv(connection_socket, buffer, buffer_len, flags);
 
-	if (arrlen(flatbuffer_data) > 0)
-	{
-		printf("We've got some data! Data Length: %td Packets Read: %i\n", arrlen(flatbuffer_data), packets_read);
+			// Less than zero is an error
+			if (current_bytes_read < 0)
+			{
+				printf("recv_error: %s\n", strerror(errno));
+				exit(0);
+			}
+
+			// No bytes read this iteration. Try again
+			if (current_bytes_read == 0)
+		  	{
+				continue;
+			}
+
+			// current_bytes_read > 0, we've got data!
+			if (current_bytes_read > 0)
+		  	{
+				// Flatbuffer size will be prefixed to flatbuffer data. Set it when we encounter it
+				if (!flatbuffer_size)
+				{
+					assert(current_bytes_read >= sizeof(flatbuffers::uoffset_t));
+					flatbuffer_size = *(flatbuffers::uoffset_t*)(buffer);
+				}
+
+				total_bytes_read += current_bytes_read;
+				i32 next_idx = arrlen(flatbuffer_data);
+				arraddn(flatbuffer_data, current_bytes_read);
+				memcpy(&flatbuffer_data[next_idx], buffer, current_bytes_read);
+				++packets_read;	
+			}
+		}
+		while (current_bytes_read == 0 || (flatbuffer_size && total_bytes_read < flatbuffer_size.value()));
+
+		if (arrlen(flatbuffer_data) > 0)
+		{
+			printf("We've got some data! Data Length: %td Packets Read: %i\n", arrlen(flatbuffer_data), packets_read);
+		}
+
+		// Interpret Flatbuffer data
+		auto* update = Blender::LiveLink::GetSizePrefixedUpdate(flatbuffer_data);
+		assert(update);
+		if (auto objects = update->objects())
+		{
+			for (i32 idx = 0; idx < objects->size(); ++idx)
+			{
+				auto object = objects->Get(idx);
+				if (auto object_name = object->name())
+				{
+					printf("\tObject Name: %s\n", object_name->c_str());
+				}
+
+				int unique_id = object->unique_id();
+				bool visibility = object->visibility();
+
+				auto object_location = object->location();
+				if (!object_location)
+				{
+					continue;
+				}
+
+				auto object_scale = object->scale();
+				if (!object_scale)
+				{
+					continue;
+				}
+
+				auto object_rotation = object->rotation();
+				if (!object_rotation)
+				{
+					continue;
+				}
+
+
+				HMM_Vec4 location = HMM_V4(object_location->x(), object_location->y(), object_location->z(), 1);
+				HMM_Vec4 scale = HMM_V4(object_scale->x(), object_scale->y(), object_scale->z(), 0);
+				HMM_Quat rotation = HMM_Q(object_rotation->x(), object_rotation->y(), object_rotation->z(), object_rotation->w());
+
+				if (state.objects.contains(unique_id))
+				{
+					//TODO: just clean up old object for now. Eventually we'll modify in-place
+
+					//TODO:
+					continue;
+
+					//TODO: Need to handle race condition with rendering code
+					Object& existing_object = state.objects[unique_id];
+					sg_destroy_buffer(existing_object.storage_buffer);
+					if (existing_object.has_mesh)
+					{
+						sg_destroy_buffer(existing_object.mesh.index_buffer);
+						sg_destroy_buffer(existing_object.mesh.vertex_buffer);
+					}
+				}
+
+				Object game_object = make_object(
+					unique_id, 
+					visibility, 
+					location, 
+					scale, 
+					rotation
+				);
+
+				//FCS TODO: Create some generic "game object" and then add mesh/light/etc. data based on existence of that data in flatbuffer
+				if (auto object_mesh = object->mesh())
+				{
+					sbuffer(Vertex) vertices = nullptr;
+					if (auto flatbuffer_vertices = object_mesh->vertices())
+					{
+						for (i32 vertex_idx = 0; vertex_idx < flatbuffer_vertices->size(); ++vertex_idx)
+						{
+							auto vertex = flatbuffer_vertices->Get(vertex_idx);
+							auto vertex_position = vertex->position();
+							auto vertex_normal = vertex->normal();
+
+							Vertex new_vertex = {
+								.position = {
+									.X = vertex_position.x(),
+									.Y = vertex_position.y(),
+									.Z = vertex_position.z(),
+									.W = vertex_position.w(),
+								},
+								.normal = {
+									.X = vertex_normal.x(),
+									.Y = vertex_normal.y(),
+									.Z = vertex_normal.z(),
+									.W = vertex_normal.w(),
+								},
+							};
+
+							arrput(vertices, new_vertex);
+						}
+					}
+
+					sbuffer(u32) indices = nullptr;
+					if (auto flatbuffer_indices = object_mesh->indices())
+					{
+						for (i32 indices_idx = 0; indices_idx < flatbuffer_indices->size(); ++indices_idx)
+						{
+							u32 new_index = flatbuffer_indices->Get(indices_idx);
+							arrput(indices, new_index);
+						}
+					}
+
+					// Set Mesh Data on Game Object
+					game_object.has_mesh = true;
+					game_object.mesh = make_mesh(vertices, arrlen(vertices), indices, arrlen(indices));
+				}
+
+				state.objects[unique_id] = game_object;
+			}
+
+			state.blender_data_loaded = true;
+		}
 	}
 
 	shutdown(connection_socket,2);
 	shutdown(state.blender_socket,2);
-
-	// Interpret Flatbuffer data
-	auto* update = Blender::LiveLink::GetUpdate(flatbuffer_data);
-	assert(update);
-	if (auto objects = update->objects())
-	{
-		for (i32 idx = 0; idx < objects->size(); ++idx)
-	   	{
-			auto object = objects->Get(idx);
-			if (auto object_name = object->name())
-			{
-				printf("\tObject Name: %s\n", object_name->c_str());
-			}
-
-			int unique_id = object->unique_id();
-
-			// TODO: Still add invisible objects as they may become visible later...
-			if(!object->visibility())
-			{
-				continue;
-			}
-
-			auto object_location = object->location();
-			if (!object_location)
-			{
-				continue;
-			}
-
-			auto object_scale = object->scale();
-			if (!object_scale)
-			{
-				continue;
-			}
-
-			auto object_rotation = object->rotation();
-			if (!object_rotation)
-			{
-				continue;
-			}
-
-
-			//FCS TODO: Create some generic "game object" and then add mesh/light/etc. data based on existence of that data in flatbuffer
-			if (auto object_mesh = object->mesh())
-			{
-				HMM_Vec4 location = HMM_V4(object_location->x(), object_location->y(), object_location->z(), 1);
-
-				HMM_Vec4 scale = HMM_V4(object_scale->x(), object_scale->y(), object_scale->z(), 0);
-
-				HMM_Quat rotation = HMM_Q(object_rotation->x(), object_rotation->y(), object_rotation->z(), object_rotation->w());
-
-				sbuffer(Vertex) vertices = nullptr;
-				if (auto flatbuffer_vertices = object_mesh->vertices())
-				{
-					for (i32 vertex_idx = 0; vertex_idx < flatbuffer_vertices->size(); ++vertex_idx)
-					{
-						auto vertex = flatbuffer_vertices->Get(vertex_idx);
-						auto vertex_position = vertex->position();
-						auto vertex_normal = vertex->normal();
-
-						Vertex new_vertex = {
-							.position = {
-								.X = vertex_position.x(),
-								.Y = vertex_position.y(),
-								.Z = vertex_position.z(),
-								.W = vertex_position.w(),
-							},
-							.normal = {
-								.X = vertex_normal.x(),
-								.Y = vertex_normal.y(),
-								.Z = vertex_normal.z(),
-								.W = vertex_normal.w(),
-							},
-						};
-
-						arrput(vertices, new_vertex);
-					}
-				}
-
-				sbuffer(u32) indices = nullptr;
-				if (auto flatbuffer_indices = object_mesh->indices())
-				{
-					for (i32 indices_idx = 0; indices_idx < flatbuffer_indices->size(); ++indices_idx)
-					{
-						u32 new_index = flatbuffer_indices->Get(indices_idx);
-						arrput(indices, new_index);
-					}
-				}
-
-				arrput(state.meshes, make_mesh(location, scale, rotation, vertices, arrlen(vertices), indices, arrlen(indices)));
-			}
-		}
-	}
-
-	state.blender_data_loaded = true;
 }
 
 void init(void) {
@@ -392,7 +461,7 @@ void frame(void)
 			.action = {
 				.colors[0] = {
 					.load_action = SG_LOADACTION_CLEAR,
-					.clear_value = { 0.25f, 0.5f, 0.75f, 1.0f }
+					.clear_value = { 0.25f, 0.25f, 0.25f, 1.0f }
 				},
 			},
 			.swapchain = sglue_swapchain()
@@ -424,14 +493,19 @@ void frame(void)
 			sg_apply_pipeline(state.pipeline);
 			sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_params, SG_RANGE(vs_params));
 
-			for (i32 mesh_idx = 0; mesh_idx < arrlen(state.meshes); ++mesh_idx)
+			for (auto const& [unique_id, object] : state.objects)
 			{
-				Mesh& mesh = state.meshes[mesh_idx];
+				if (!object.has_mesh || !object.visibility)
+				{
+					continue;
+				}
+
+				const Mesh& mesh = object.mesh;
 				sg_bindings bindings = {
-					.vertex_buffers[0] = mesh.vbuf,
-					.index_buffer = mesh.ibuf,
+					.vertex_buffers[0] = mesh.vertex_buffer,
+					.index_buffer = mesh.index_buffer,
 					.vs = {
-						.storage_buffers[SLOT_ObjectDataBuffer] = mesh.sbuf,
+						.storage_buffers[SLOT_ObjectDataBuffer] = object.storage_buffer,
 					},
 				};
 				sg_apply_bindings(&bindings);
