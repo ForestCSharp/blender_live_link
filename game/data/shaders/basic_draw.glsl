95fa0e6ad5403893468ec66e346e4a96081b5283
Forest Collins Sharp <forestcollinssharp@gmail.com>
2024-12-29 10:44:32 -0600

    Proper Point Light Sampling Function
    - intensity * cos(theta) / 4 * PI * dist_squared
    
    - also remove .DS_Store files from git

--- game/data/shaders/basic_draw.glsl
+++ game/data/shaders/basic_draw.glsl
@@ -19,10 +19,12 @@ in vec4 position;
 in vec4 normal;
 
 out vec4 world_position;
+out vec4 world_normal;
 out vec4 color;
 
 void main() {
 	world_position = object_data_array[0].model_matrix * position;
+	world_normal = object_data_array[0].model_matrix * normal;
     gl_Position = vp * world_position;
     color = vec4(1,1,1,1);
 }
@@ -31,6 +33,7 @@ void main() {
 @fs fs
 
 #define MAX_POINT_LIGHTS 100
+#define M_PI 3.1415926535897932384626433832795
 
 struct PointLight {
 	vec4 location;
@@ -49,6 +52,7 @@ layout(binding=1) readonly buffer LightsDataBuffer {
 };
 
 in vec4 world_position;
+in vec4 world_normal;
 in vec4 color;
 
 out vec4 frag_color;
@@ -59,6 +63,19 @@ float dist_squared( vec3 a, vec3 b)
     return dot(c,c);
 }
 
+float cosine_angle(vec3 a, vec3 b)
+{
+	return dot(normalize(a), normalize(b));
+}
+
+vec4 sample_point_light(PointLight in_point_light, vec3 in_world_position, vec3 in_normal, vec4 in_color)
+{
+	float numerator = in_point_light.power * cosine_angle(in_world_position, in_normal);
+	float denominator = 4 * M_PI * dist_squared(in_point_light.location.xyz, in_world_position);
+	float lighting_factor = numerator / denominator;
+	return in_color * in_point_light.color * lighting_factor;
+}
+
 void main() {
 	LightsData lights_data = light_data_array[0];
 
@@ -66,8 +83,7 @@ void main() {
 	for (int i = 0; i < lights_data.num_point_lights; ++i)
 	{
 		PointLight point_light = lights_data.point_lights[i];
-		float lighting_factor = point_light.power / dist_squared(point_light.location.xyz, world_position.xyz);
-		final_color += (color * point_light.color * lighting_factor);
+		final_color += sample_point_light(point_light, world_position.xyz, world_normal.xyz, color);
 	}
 
 	frag_color = final_color;
